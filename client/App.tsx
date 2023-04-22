03fc73435ea22ab3e0c8fdc43698db3c3895272e
Sean Casey <seancasey076@gmail.com>
2023-04-22 15:18:48 -0500

    init setup of client

---
+++ client/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorld = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Text>Hello, React Native!!! :D </Text>
+
+    </View>
+  )
+
+}
+
+export default HelloWorld;
