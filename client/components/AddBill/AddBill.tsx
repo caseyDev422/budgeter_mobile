22d7b30a30bd210741934002f14326c4fd7aafb4
Matthew Casey <mattcasey879@gmail.com>
2023-06-22 16:46:33 -0500

    Add Bill form(WIP)

--- client/components/AddBill/AddBill.tsx
+++ client/components/AddBill/AddBill.tsx
@@ -1,9 +1,12 @@
 import { ParamListBase, RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { Text } from "@rneui/base";
-import React from "react";
+import React, { SetStateAction, useState } from "react";
 import { RootStackParamList } from "../../models/types/RootStackParamList.type";
-
+import { View } from "react-native";
+import AddBillStyles from "../../styles/AddBillStyleSheet";
+import {  Input, Text } from "react-native-elements";
+import { Checkbox } from 'react-native-paper';
+import { DatePickerInput } from "react-native-paper-dates";
 interface AddBillProps {
   navigation: StackNavigationProp<RootStackParamList>;
   route: RouteProp<ParamListBase>;
@@ -11,8 +14,27 @@ interface AddBillProps {
 }
 
 const AddBill = (props: AddBillProps) => {
+  const [date, setDate] = useState(undefined)
+  const [checked, setChecked] = useState(false);
   return (
-   <Text>AddBill Works!</Text>
+   <View style={AddBillStyles.container}>
+    <Input placeholder="Bill Name">
+    </Input>
+    <Input placeholder="Amount"></Input>
+    <DatePickerInput
+      inputMode="start"
+      locale="en"
+      label="Due Date"
+      value={date}
+      onChange={(d) => {
+        setDate(d as SetStateAction<undefined>)
+      }}
+    />
+    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+    <Checkbox color={'#1976d2'} onPress={() => setChecked(!checked)} status={checked ? 'checked' : 'unchecked'}/>
+    <Text>Auto Draft</Text>
+    </View>
+   </View>
   )
 
 }
---
+++ client/styles/AddBillStyleSheet.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+
+
+const AddBillStyles = StyleSheet.create({
+  container: {
+    flex: 0.75,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '75%',
+  }
+
+});
+
+
+
+export default AddBillStyles
